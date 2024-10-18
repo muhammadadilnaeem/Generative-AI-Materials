586376ded185b4ea730e323bef80ee82831461df
adil <madilnaeem0@gmail.com>
2024-10-18 17:01:43 +0500

    project setup

---
+++ Generative AI Mini Projects/02_source_code_analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot Assistant",
+    version="0.0.1",
+    author='Muhammad Adil Naeem',
+    author_email='madilnaeem0@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
